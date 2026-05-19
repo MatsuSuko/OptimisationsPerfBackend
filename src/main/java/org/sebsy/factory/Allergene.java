54aa55237a8749cac83848280404f797b17556db
Souvanny <souvannyb21@gmail.com>
2026-05-19 16:20:36 +0200

    ajout des classes modèles du pattern Factory Method

---
+++ src/main/java/org/sebsy/factory/Allergene.java
@@ -0,0 +1,8 @@
+package org.sebsy.factory;
+
+public class Allergene extends Element {
+
+    public Allergene(String nom, double valeur, Unite unite) {
+        super(nom, valeur, unite);
+    }
+}
---
+++ src/main/java/org/sebsy/factory/Ingredient.java
@@ -0,0 +1,8 @@
+package org.sebsy.factory;
+
+public class Ingredient extends Element {
+
+    public Ingredient(String nom, double valeur, Unite unite) {
+        super(nom, valeur, unite);
+    }
+}
